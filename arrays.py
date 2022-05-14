796c43f1962ce07b185028ab2542af8465c98e6a
Rifat Jamil <rifatjamil54@gmail.com>
2022-05-14 19:49:33 +0600

    commit

---
+++ arrays.py
@@ -0,0 +1,14 @@
+new_list = [1, 2, 3, 4, 5]
+
+result1 = new_list[0] #access index 0 
+
+print(result1) # we found 1 on index 0 
+
+result2 = new_list[7] #access index 7
+
+print(result2) # IndexError: list index(index 7) out of range
+
+
+
+
+
