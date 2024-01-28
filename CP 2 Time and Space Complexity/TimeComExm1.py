a2410a82fdfaeaeb2d37893af2b71dca62ce6d6d
Refat Jamil <rifatjamil54@gmail.com>
2024-01-28 13:10:41 +0600

    time and space complaxity

--- CP 2 Time and Space Complexity/TimeComExm1.py
+++
@@ -1,9 +0,0 @@
-number_first = 10 # O(1)
-number_second = 20 # O(1)
-result = number_first + number_second # O(1)
-print(result) # O(1)
-
-"""
-Time Complexity
-O(1)
-"""
--- CP 2 Time and Space Complexity/TimeComExm2.py
+++
@@ -1,5 +0,0 @@
-n = input()
-n = int(n)
-result = n*(n+1)/2
-print(result)
-
